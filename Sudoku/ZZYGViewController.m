7b776480ccbfd14d209672a8f86f3cca50d96a78
gyxlucy <gyxlucy@gmail.com>
2014-09-12 11:15:13 -0700

    initial

--- Sudoku/ZZYGViewController.m
+++ Sudoku/ZZYGViewController.m
@@ -7,8 +7,26 @@
 //
 
 #import "ZZYGViewController.h"
+#import "ZZYGGrid.h"
 
-@interface ZZYGViewController ()
+int initialGrid[9][9] = {
+    {8,0,0,4,2,0,0,0,9},
+    {0,0,9,5,0,0,0,0,4},
+    {0,2,0,6,9,0,5,0,0},
+    {6,5,0,0,0,0,4,3,0},
+    {0,8,0,0,0,6,0,0,7},
+    {0,1,0,0,4,5,6,0,0},
+    {0,0,0,8,6,0,0,0,2},
+    {3,4,0,9,0,0,1,0,0},
+    {8,0,0,3,0,2,7,4,0}
+};
+
+UIButton* myButtons[9][9];
+
+@interface ZZYGViewController () {
+    UIView* _gridView;
+    UIButton* _button;
+}
 
 @end
 
@@ -18,12 +36,74 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // create grid frame
+    CGRect frame = self.view.frame;
+    CGFloat x = CGRectGetWidth(frame)*.1;
+    CGFloat y = CGRectGetHeight(frame)*.1;
+    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame))*.80;
+    
+    CGRect gridFrame = CGRectMake(x, y, size, size);
+    
+    // create grid view
+    _gridView = [[ZZYGGrid alloc] initWithFrame:gridFrame];
+    _gridView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:_gridView];
+    
+    CGFloat buttonSize = size / 10.0;
+    
+    [self addButtons:buttonSize];
+
+}
+
+- (void)addButtons:(CGFloat) buttonSize
+{
+    // create button
+    
+    int row = 14;
+    int column;
+    
+    for (int i = 0; i < 9; i++) {
+        column = 14;
+        for (int j = 0; j < 9; j++) {
+            
+            CGRect buttonFrame = CGRectMake(row, column, buttonSize, buttonSize);
+            myButtons[i][j] = [[UIButton alloc] initWithFrame:buttonFrame];
+            myButtons[i][j].backgroundColor = [UIColor whiteColor];
+            if (initialGrid[i][j]){
+                [myButtons[i][j] setTitle:[NSString stringWithFormat: @"%d", initialGrid[i][j]] forState:UIControlStateNormal];
+            }
+            [myButtons[i][j] setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+            [_gridView addSubview:myButtons[i][j]];
+            
+            // create target for button
+            [myButtons[i][j] addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+            
+            column += (buttonSize + 3);
+            if ((j + 1) % 3 == 0) {
+                column += 5;
+            }
+        }
+        row += (buttonSize + 3);
+        if ((i + 1) % 3 == 0) {
+            row += 5;
+        }
+    }
+    
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)buttonPressed:(UIButton*)sender
+{
+    NSLog([@"You Pressed the button!" stringByAppendingString: sender.currentTitle]);
 }
 
 @end
